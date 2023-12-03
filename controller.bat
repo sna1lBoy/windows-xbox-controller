0da364a91cb454da41d79ca0631c77d65212190d
sna1lBoy <133536934+sna1lBoy@users.noreply.github.com>
2023-12-03 13:02:58 -0600

    adjusted pathing

--- controller.bat
+++ controller.bat
@@ -3,11 +3,8 @@
 cd files\bin
 powershell Start-Process -FilePath '.\controller.exe' -Verb RunAs -WindowStyle hidden
 
-cd ..
-cd ..
-cd ..
-cd ..
+cd ..\..\..
 
 powershell -Command Invoke-WebRequest -Uri https://github.com/sna1lBoy/windows-xbox-controller/archive/refs/heads/main.zip -OutFile main.zip
-powershell -Command Expand-Archive -Path .\main.zip -DestinationPath .\windows-xbox-controller-main -Force
+powershell -Command Expand-Archive -Path .\main.zip -DestinationPath .\ -Force
 powershell -Command Remove-Item -Path ".\main.zip"
